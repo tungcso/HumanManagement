3518346efc09db62973a3c8f46156a05084d3df5
tungcso <luongtung882005@gmail.com>
2026-02-02 19:44:09 +0700

    fix Code

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { clearUser, getAT, setAT } from "./AuthToken";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080/api",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "https://humanmanagement.onrender.com",
   withCredentials: true,
 });
 
